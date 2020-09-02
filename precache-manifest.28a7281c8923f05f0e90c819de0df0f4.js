5280e44e121b3a3c3359b403aeed98eaf7dadf3f
Volodymyr <vov2706@gmail.com>
2020-09-02 18:01:41 +0300

    Updates

---
+++ precache-manifest.28a7281c8923f05f0e90c819de0df0f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08ecf339d0c348c058869ebd53c6d21",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "b3a3fa788a90005d35a5",
+    "url": "/expense-tracker/static/css/main.94ad8818.chunk.css"
+  },
+  {
+    "revision": "7d6976a867b9dbdb8773",
+    "url": "/expense-tracker/static/js/2.876d807e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.876d807e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a3fa788a90005d35a5",
+    "url": "/expense-tracker/static/js/main.7d354cf8.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
